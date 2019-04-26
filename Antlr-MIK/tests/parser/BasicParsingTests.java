a9eea1b9bea69f934b257ca8ce655ca39671ecc9
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-04-26 09:49:03 +0200

    Refractured structure of unit tests and added more.

--- Antlr-MIK/tests/parser/BasicParsingTests.java
+++ Antlr-MIK/tests/parser/BasicParsingTests.java
@@ -8,34 +8,21 @@ public class BasicParsingTests {
     private static TacticLexer lexer;
     private static Tactic parser;
 
-    @BeforeClass
-    public static void beforeAllTests(){
-        //lexer = new TacticLexer(new ANTLRInputStream("int i = 5;")); //The string should properly be an empty string? Why is that now allowed?
-        lexer = new TacticLexer(new ANTLRInputStream("int i = 5;")); //The string should properly be an empty string? Why is that now allowed?
-        parser = new Tactic(new CommonTokenStream(lexer));
-        //parser.addParseListener(); //Not needed for syntax checking
-    }
-
-    @Before
-    public void beforeEveryTest(){
-        parser.reset();
-    }
-
     @After
     public void afterEveryTest(){
-        parser.setInputStream(new CommonTokenStream(lexer));
+        parser = new Tactic(new CommonTokenStream(lexer));
         parser.start();
         Assert.assertEquals(0, parser.getNumberOfSyntaxErrors());
     }
 
-    /*
     @Test
     public void testTest(){
 
         //CharStream input = new ANTLRFileStream("FILENAME");
 
-        ANTLRInputStream input = new ANTLRInputStream("int i = 5;");
-        TacticLexer lexer = new TacticLexer(input);
+        //ANTLRInputStream input = new ANTLRInputStream("int i = 5;");
+        //TacticLexer lexer = new TacticLexer(input);
+        TacticLexer lexer = new TacticLexer(new ANTLRInputStream("int i = 5;"));
         Tactic parser = new Tactic(new CommonTokenStream(lexer));
         //parser.addParseListener(); //Not needed for syntax checking
         parser.start();
@@ -44,10 +31,45 @@ public class BasicParsingTests {
 
         //Hint: if you want to re-use the parser+lexer instances, call their 'reset()' methods after setting their input streams.
         //https://stackoverflow.com/questions/18110180/processing-a-string-with-antlr4
-    }*/
+    }
 
     @Test
     public void testTest2(){
-        lexer.setInputStream(new ANTLRInputStream("int i = 5"));
+        lexer = new TacticLexer(new ANTLRInputStream("int i = 5;"));
     }
+
+    @Test
+    public void backgroundImage01() {
+        lexer = new TacticLexer(new ANTLRInputStream("levels[0] = " + "\"" + "\\some\\path\\pictures1.jpg" + "\"" + ";"));
+    }
+
+    @Test
+    public void declaration01() {
+        lexer = new TacticLexer(new ANTLRInputStream("int tempInt;"));
+    }
+
+    @Test
+    public void declaration02() {
+        lexer = new TacticLexer(new ANTLRInputStream("float tempFloat;"));
+    }
+
+    @Test
+    public void declaration03() {
+        lexer = new TacticLexer(new ANTLRInputStream("bool tempBool;"));
+    }
+    @Test
+    public void declaration04() {
+        lexer = new TacticLexer(new ANTLRInputStream("stringTemp;"));
+    }
+
+    @Test
+    public void declaration05() {
+        lexer = new TacticLexer(new ANTLRInputStream("vector tempVector;"));
+    }
+    @Test
+    public void declaration06() {
+        lexer = new TacticLexer(new ANTLRInputStream("GamePiece tempGamePiece;"));
+    }
+
+
 }
