855b2f5f07d22de235d1aa33d4be685579e37d9f
unknown <flintfedt-trash@hotmail.com>
2019-04-26 14:40:58 +0200

    progression

--- Antlr-MIK/src/MainClass.java
+++ Antlr-MIK/src/MainClass.java
@@ -1,6 +1,7 @@
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.IOException;
 import java.util.logging.Level;
@@ -15,7 +16,11 @@ public class MainClass {
             TacticLexer lexer = new TacticLexer(input);  //Create the lexer
             Tactic parser = new Tactic(new CommonTokenStream(lexer)); //Create the parser
             //parser.addParseListener(new TacTypesCheckingLangCustomListener()); //Attach listener. This is done to run code on steps of the tree walk
-            parser.start(); // Run the parser
+            //parser.start(); // Run the parser
+
+            //https://stackoverflow.com/questions/15050137/once-grammar-is-complete-whats-the-best-way-to-walk-an-antlr-v4-tree
+            //Walk the tree multiple times with different listeners
+            //ParseTree tree = parser.component();
 
         } catch (IOException ex){
             Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex); //Reports if no file was found.
---
+++ Antlr-MIK/src/TypeCheckerListener.java
@@ -0,0 +1,2 @@
+public class TypeCheckerListener extends TacticBaseListener {
+}
