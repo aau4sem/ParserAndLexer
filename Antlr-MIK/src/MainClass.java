954f17fe8ad7d535d3a964d013ec5f5da36e60e8
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-04-29 14:28:01 +0200

    Changed MainClass to run mutiple runs through tree.

--- Antlr-MIK/src/MainClass.java
+++ Antlr-MIK/src/MainClass.java
@@ -12,15 +12,36 @@ public class MainClass {
     /** This method will run the entire compiler. */
     public static void main(String[] args) {
         try{
+            /* Simple main example:
             CharStream input = new ANTLRFileStream("compilerInput.tac"); //Load an input to compile
             TacticLexer lexer = new TacticLexer(input);  //Create the lexer
             Tactic parser = new Tactic(new CommonTokenStream(lexer)); //Create the parser
-            //parser.addParseListener(new TacTypesCheckingLangCustomListener()); //Attach listener. This is done to run code on steps of the tree walk
-            //parser.start(); // Run the parser
+            parser.addParseListener(new TypeCheckerListener()); //Attach listener. This is done to run code on steps of the tree walk
+            parser.start(); // Run the parser
+            */
+
+            //Main example with multiple listeners/parses:
+            CharStream input = new ANTLRFileStream("compilerInput.tac"); //Load an input to compile
+            TacticLexer lexer = new TacticLexer(input);  //Create the lexer
+            Tactic parser = new Tactic(new CommonTokenStream(lexer)); //Create the parser
+            //Listener 1
+            TypeCheckerListener listener1 = new TypeCheckerListener();
+            parser.addParseListener(listener1); //Attach listener. This is done to run code on steps of the tree walk
+            parser.start(); // Run the parser
+            parser.removeParseListener(listener1);
+            //Listener 2
+            parser.reset();
+            TestListener listener2 = new TestListener();
+            parser.addParseListener(listener2);
+            parser.start();
+
+
 
             //https://stackoverflow.com/questions/15050137/once-grammar-is-complete-whats-the-best-way-to-walk-an-antlr-v4-tree
             //Walk the tree multiple times with different listeners
             //ParseTree tree = parser.component();
+
+            // MAYBE THIS STRUCTURE: CREATE THE TREE -> COLLECT VARIABLES AND FUNCTIONS with scope data -> type checking -> semantics check?
 
         } catch (IOException ex){
             Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex); //Reports if no file was found.
