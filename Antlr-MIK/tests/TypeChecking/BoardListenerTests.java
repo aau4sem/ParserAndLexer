41bcbd114f49db1ae238535d12133dbfac3dd599
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-04-30 23:58:50 +0200

    Added a single test for both ActionCollectorListener and BoardListener.

---
+++ Antlr-MIK/tests/TypeChecking/BoardListenerTests.java
@@ -0,0 +1,32 @@
+package TypeChecking;
+
+import CustomListeners.BoardListener;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.*;
+import gen.*;
+
+public class BoardListenerTests {
+
+    private static TacticLexer lexer;
+    private static Tactic parser;
+
+    @Test
+    public void overallTest01(){
+        lexer = new TacticLexer(new ANTLRInputStream(
+                "board_level[0] = \"TestZero\";" +
+                "board_level[4] = \"TestFour\";" +
+                "board_level[1] = \"TestOne\";"));
+        parser = new Tactic(new CommonTokenStream(lexer));
+        BoardListener bl = new BoardListener();
+        parser.addParseListener(bl);
+        parser.prog();
+        Assert.assertEquals(0, parser.getNumberOfSyntaxErrors());
+
+        Assert.assertEquals(5, bl.getBoardPaths().length);
+        Assert.assertEquals("TestZero", bl.getBoardPaths()[0]);
+        Assert.assertEquals("TestOne", bl.getBoardPaths()[1]);
+        Assert.assertEquals("TestFour", bl.getBoardPaths()[4]);
+        Assert.assertNull(bl.getBoardPaths()[2]);
+    }
+}
