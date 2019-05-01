53fcd0410206fb526842b2d8f1fb56f65704c692
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-05-01 11:15:24 +0200

    Create TypeCheckerHelperTests.java

---
+++ Antlr-MIK/tests/typeChecking/utils/TypeCheckerHelperTests.java
@@ -0,0 +1,30 @@
+package typeChecking.utils;
+
+import customListeners.ActionCollectorListener;
+import customListeners.VariableCollectorListener;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.*;
+import gen.*;
+
+public class TypeCheckerHelperTests {
+
+    private static TacticLexer lexer;
+    private static Tactic parser;
+
+    @Test
+    public void overallTest01(){
+        lexer = new TacticLexer(new ANTLRInputStream("GamePiece one; GamePiece two; GamePiece three;" +
+                "Move(one, (2,3,4), 2); Wait(one, 6); Move(two, (2,3,4), 2); Move(two, (2,3,5), 6);" +
+                "Wait(three, 1); Wait(three, 3);"));
+        parser = new Tactic(new CommonTokenStream(lexer));
+        VariableCollectorListener vcl = new VariableCollectorListener();
+        ActionCollectorListener acl = new ActionCollectorListener(vcl);
+        parser.addParseListener(vcl);
+        parser.addParseListener(acl);
+        parser.prog();
+        Assert.assertEquals(0, parser.getNumberOfSyntaxErrors());
+
+        Assert.assertEquals(6, acl.getActionFunctions().size());
+    }
+}
