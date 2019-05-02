58f51778312fbf9850a4fa412699deb75cf54242
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-05-02 23:20:18 +0200

    Added tests for GamePiece parsing and changing properties.

---
+++ Antlr-MIK/tests/typeChecking/GamePieceParsingTests.java
@@ -0,0 +1,84 @@
+package typeChecking;
+
+import customListeners.ActionCollectorListener;
+import customListeners.VariableCollectorListener;
+import gen.Tactic;
+import gen.TacticLexer;
+import model.dataTypes.GamePiece;
+import model.dataTypes.Vector;
+import model.utils.TypeCheckerHelper;
+import model.variables.VariableContainer;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GamePieceParsingTests {
+
+    private static TacticLexer lexer;
+    private static Tactic parser;
+
+    @Test
+    public void propertyDefaultValues01(){
+        lexer = new TacticLexer(new ANTLRInputStream("GamePiece gp;"));
+        parser = new Tactic(new CommonTokenStream(lexer));
+        VariableCollectorListener vcl = new VariableCollectorListener();
+        parser.addParseListener(vcl);
+        parser.prog();
+        Assert.assertEquals(0, parser.getNumberOfSyntaxErrors());
+
+        VariableContainer varCon = vcl.getValueFromIdentifier("gp");
+        Assert.assertNotNull(varCon);
+
+        GamePiece gp = TypeCheckerHelper.parseGamePiece(varCon.getValue());
+        Assert.assertNotNull(gp);
+
+        Assert.assertNull(gp.getPosition());
+        Assert.assertEquals(1f, gp.getSize().floatValue(), 4);
+        Assert.assertNull(gp.getColor());
+        Assert.assertEquals("", gp.getLabel());
+        Assert.assertEquals(1f, gp.getOpacity(), 4);
+        Assert.assertEquals("circle", gp.getShape());
+    }
+
+    @Test
+    public void propertyChangeVector01(){
+        lexer = new TacticLexer(new ANTLRInputStream("GamePiece gp; gp.position = (2,3,1);"));
+        parser = new Tactic(new CommonTokenStream(lexer));
+        VariableCollectorListener vcl = new VariableCollectorListener();
+        parser.addParseListener(vcl);
+        parser.prog();
+        Assert.assertEquals(0, parser.getNumberOfSyntaxErrors());
+
+        VariableContainer varCon = vcl.getValueFromIdentifier("gp");
+        Assert.assertNotNull(varCon);
+
+        GamePiece gp = TypeCheckerHelper.parseGamePiece(varCon.getValue());
+        Assert.assertNotNull(gp);
+
+        Vector position = gp.getPosition();
+        Assert.assertNotNull(position);
+
+        Assert.assertEquals(2, position.getX());
+        Assert.assertEquals(3, position.getY());
+        Assert.assertEquals(1, position.getZ());
+    }
+
+    @Test
+    public void propertyChangeSize01(){
+        lexer = new TacticLexer(new ANTLRInputStream("GamePiece gp; gp.size = 2.56;"));
+        parser = new Tactic(new CommonTokenStream(lexer));
+        VariableCollectorListener vcl = new VariableCollectorListener();
+        parser.addParseListener(vcl);
+        parser.prog();
+        Assert.assertEquals(0, parser.getNumberOfSyntaxErrors());
+
+        VariableContainer varCon = vcl.getValueFromIdentifier("gp");
+        Assert.assertNotNull(varCon);
+
+        GamePiece gp = TypeCheckerHelper.parseGamePiece(varCon.getValue());
+        Assert.assertNotNull(gp);
+
+        Assert.assertEquals(2.56f, gp.getSize().floatValue());
+    }
+}
