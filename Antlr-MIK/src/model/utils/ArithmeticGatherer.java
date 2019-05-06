76b39ad9bec064b9e8007473b1330a38b9a52185
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-05-06 13:06:01 +0200

    ArithmeticGatherer: added a getResult method and an evaluation method that converts the equation into a result (String -> value). And tests for that.

--- Antlr-MIK/src/model/utils/ArithmeticGatherer.java
+++ Antlr-MIK/src/model/utils/ArithmeticGatherer.java
@@ -1,6 +1,5 @@
 package model.utils;
 
-import gen.Tactic;
 import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.Token;
@@ -9,12 +8,8 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeVisitor;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
-import java.util.ArrayList;
-
 /** This class is used when evaluating an arithmetic expression. */
 public class ArithmeticGatherer implements TerminalNode {
-
-    private ArrayList<Tactic.NumberContext> values = new ArrayList<>();
 
     private StringBuilder equation = new StringBuilder();
 
@@ -22,9 +17,98 @@ public class ArithmeticGatherer implements TerminalNode {
         equation.append(val);
     }
 
+    /** @return the result of the equation. */
+    public double getResult(){
+        return (getEquation().length() == 0) ? 0 : eval(getEquation());
+    }
+
     public String getEquation(){
         return equation.toString();
     }
+
+    //https://stackoverflow.com/questions/3422673/how-to-evaluate-a-math-expression-given-in-string-form
+    /** @return the result of the equation. */
+    public static double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if      (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if      (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char)ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
+    }
+
+    // NOT USED --------------------------------------------------------------------
 
     @Override
     public ParseTree getParent() {
