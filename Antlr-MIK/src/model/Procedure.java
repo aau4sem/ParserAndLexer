4d9ab2a5c3e7407f170eb2432ec3c5cab3521bc3
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-05-09 12:22:21 +0200

    Procedure: Added a collection for statments.

--- Antlr-MIK/src/model/Procedure.java
+++ Antlr-MIK/src/model/Procedure.java
@@ -1,23 +1,43 @@
 package model;
 
+import customListeners.VariableCollectorListener;
+import gen.Tactic;
+import model.utils.Argument;
 import model.utils.Parameter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Procedure {
 
     private ArrayList<Parameter> parameters;
+    private ArrayList<Tactic.StmtContext> statements;
 
-    public Procedure(ArrayList<Parameter> parameters) {
+    public Procedure(ArrayList<Parameter> parameters, ArrayList<Tactic.StmtContext> statements) {
         this.parameters = parameters;
+        this.statements = statements;
     }
 
     public Procedure() {
-        this(new ArrayList<>());
+        this(new ArrayList<>(), new ArrayList<>());
+    }
+
+    /** executes the procedure.
+     * @param vcl used for accessing variables in the main scope. */
+    public void execute(ArrayList<Argument> arguments, VariableCollectorListener vcl){ //TODO
+        //TODO USE ALL THE OVERWRITTEN METHODS ALREADY CREATED!!!
     }
 
     public void addArgument(Parameter parameter){
         parameters.add(parameter);
+    }
+
+    public void addStatement(Tactic.StmtContext statement){
+        statements.add(statement);
+    }
+
+    public void addAllStatments(List<Tactic.StmtContext> stmtContexts){
+        statements.addAll(stmtContexts);
     }
 
     public ArrayList<Parameter> getArguments() {
