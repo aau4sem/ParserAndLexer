ba43c222c8f56a19ea209df70795dcb8e02289f1
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-05-11 16:53:05 +0200

    Reworked ArgumentType so that NUMBER now is either an Integer or Float.

--- Antlr-MIK/src/model/Utils/Argument.java
+++ Antlr-MIK/src/model/Utils/Argument.java
@@ -2,7 +2,7 @@ package model.utils;
 
 public class Argument {
 
-    public enum ArgumentType { IDENTIFIER, NUMBER, BOOL, STRING, VECTOR, GAMEPIECE_PROPERTY}
+    public enum ArgumentType { IDENTIFIER, INTEGER, FLOAT, BOOL, STRING, VECTOR, GAMEPIECE_PROPERTY}
 
     private String value;
     private ArgumentType type;
