db2b874a4a00c7b3a40d635c6286ff2964ab0101
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-05-17 15:40:03 +0200

    ArrayParsingTests: made tests complete - "all" cases has tests.

--- Antlr-MIK/tests/typeChecking/ArrayParsingTests.java
+++ Antlr-MIK/tests/typeChecking/ArrayParsingTests.java
@@ -229,6 +229,138 @@ public class ArrayParsingTests {
     }
 
     @Test
+    public void assignment07(){
+        parse("float[4] i; i[2] = 2;;");
+
+        VariableContainer i = vcl.getArrayValueFromScope("i");
+
+        Assert.assertNotNull(i);
+        Assert.assertTrue(i.isArray());
+
+        //Check default values
+        Float[] array = TypeCheckerHelper.parseFloatArray(i.getValue());
+        Assert.assertEquals(4, array.length);
+        Assert.assertEquals(1f, array[0], 10);
+        Assert.assertEquals(1f, array[1], 10);
+        Assert.assertEquals(2f, array[2], 10);
+        Assert.assertEquals(1f, array[3], 10);
+    }
+
+    @Test
+    public void assignment08(){
+        parse("float[4] i; i[2] = 2.0 + 2.0;;");
+
+        VariableContainer i = vcl.getArrayValueFromScope("i");
+
+        Assert.assertNotNull(i);
+        Assert.assertTrue(i.isArray());
+
+        //Check default values
+        Float[] array = TypeCheckerHelper.parseFloatArray(i.getValue());
+        Assert.assertEquals(4, array.length);
+        Assert.assertEquals(1f, array[0], 10);
+        Assert.assertEquals(1f, array[1], 10);
+        Assert.assertEquals(4f, array[2], 10);
+        Assert.assertEquals(1f, array[3], 10);
+    }
+
+    @Test
+    public void assignment09(){
+        parse("bool[4] i; i[1] = 2 > 5;;");
+
+        VariableContainer i = vcl.getArrayValueFromScope("i");
+
+        Assert.assertNotNull(i);
+        Assert.assertTrue(i.isArray());
+
+        //Check default values
+        Boolean[] array = TypeCheckerHelper.parseBooleanArray(i.getValue());
+        Assert.assertEquals(4, array.length);
+        Assert.assertTrue(array[0]);
+        Assert.assertFalse(array[1]);
+        Assert.assertTrue(array[2]);
+        Assert.assertTrue(array[3]);
+    }
+
+    @Test
+    public void assignment10(){
+        parse("vector[4] i; i[2] = (5,5,5) + (5,5,5);;");
+
+        VariableContainer i = vcl.getArrayValueFromScope("i");
+
+        Assert.assertNotNull(i);
+        Assert.assertTrue(i.isArray());
+
+        //Check default values
+        Vector[] array = TypeCheckerHelper.parseVectorArray(i.getValue());
+        Assert.assertEquals(4, array.length);
+        Assert.assertTrue(isVectorsValuesEqual(array[0], new Vector(0,0,0)));
+        Assert.assertTrue(isVectorsValuesEqual(array[1], new Vector(0,0,0)));
+        Assert.assertTrue(isVectorsValuesEqual(array[2], new Vector(10,10,10)));
+        Assert.assertTrue(isVectorsValuesEqual(array[3], new Vector(0,0,0)));
+    }
+
+    @Test
+    public void assignment11(){
+        parse("int[4] i; int[1] x; x[0] = 10; i[3] = x[0];;");
+
+        VariableContainer i = vcl.getArrayValueFromScope("i");
+        VariableContainer x = vcl.getArrayValueFromScope("x");
+
+        Assert.assertNotNull(i);
+        Assert.assertNotNull(x);
+        Assert.assertTrue(i.isArray());
+        Assert.assertTrue(x.isArray());
+
+        Integer[] array1 = TypeCheckerHelper.parseIntegerArray(i.getValue());
+        Assert.assertEquals(4, array1.length);
+        Assert.assertEquals(1, array1[0].intValue());
+        Assert.assertEquals(1, array1[1].intValue());
+        Assert.assertEquals(1, array1[2].intValue());
+        Assert.assertEquals(10, array1[3].intValue());
+
+        Integer[] array2 = TypeCheckerHelper.parseIntegerArray(i.getValue());
+        Assert.assertEquals(1, array2.length);
+        Assert.assertEquals(10, array2[0].intValue());
+    }
+
+    @Test
+    public void assignment12(){
+        parse("int[4] i; i[3] = i.length;;");
+
+        VariableContainer i = vcl.getArrayValueFromScope("i");
+
+        Assert.assertNotNull(i);
+        Assert.assertTrue(i.isArray());
+
+        //Check default values
+        Integer[] array = TypeCheckerHelper.parseIntegerArray(i.getValue());
+        Assert.assertEquals(4, array.length);
+        Assert.assertEquals(1, array[0].intValue());
+        Assert.assertEquals(1, array[1].intValue());
+        Assert.assertEquals(1, array[2].intValue());
+        Assert.assertEquals(4, array[3].intValue());
+    }
+
+    @Test
+    public void assignment13(){
+        parse("int[4] i; int x; x = 10; i[3] = x;;");
+
+        VariableContainer i = vcl.getArrayValueFromScope("i");
+
+        Assert.assertNotNull(i);
+        Assert.assertTrue(i.isArray());
+
+        //Check default values
+        Integer[] array = TypeCheckerHelper.parseIntegerArray(i.getValue());
+        Assert.assertEquals(4, array.length);
+        Assert.assertEquals(1, array[0].intValue());
+        Assert.assertEquals(1, array[1].intValue());
+        Assert.assertEquals(1, array[2].intValue());
+        Assert.assertEquals(10, array[3].intValue());
+    }
+
+    @Test
     public void length01(){
         parse("int[4] i; int x; x = i.length;;");
 
@@ -298,5 +430,155 @@ public class ArrayParsingTests {
         Assert.assertSame(x.getType(), VariableCollectorListener.VariableType.INT);
 
         Assert.assertEquals(4, TypeCheckerHelper.parseInt(x.getValue()).intValue());
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_int01(){
+        parse("int[4] x; x[2] = 2.0;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_int02(){
+        parse("int[4] x; x[2] = \"test\";;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_int03(){
+        parse("int[4] x; x[2] = true;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_int04(){
+        parse("int[4] x; GamePiece gp; x[2] = gp;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_int05(){
+        parse("int[4] x; x[2] = (2,2,3);;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_float02(){
+        parse("float[4] x; x[2] = \"test\";;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_float03(){
+        parse("float[4] x; x[2] = true;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_float04(){
+        parse("float[4] x; GamePiece gp; x[2] = gp;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_float05(){
+        parse("float[4] x; x[2] = (2,2,3);;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_string01(){
+        parse("string[4] x; x[2] = 1;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_string02(){
+        parse("string[4] x; x[2] = 2.0;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_string03(){
+        parse("string[4] x; x[2] = true;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_string04(){
+        parse("string[4] x; GamePiece gp; x[2] = gp;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_string05(){
+        parse("string[4] x; x[2] = (2,2,3);;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_boolean01(){
+        parse("bool[4] x; x[2] = 1;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_boolean02(){
+        parse("bool[4] x; x[2] = 2.0;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_boolean03(){
+        parse("bool[4] x; x[2] = \"test\";;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_boolean04(){
+        parse("bool[4] x; GamePiece gp; x[2] = gp;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_bool05(){
+        parse("bool[4] x; x[2] = (2,2,3);;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_GamePiece01(){
+        parse("GamePiece[4] x; x[2] = 1;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_GamePiece02(){
+        parse("GamePiece[4] x; x[2] = 2.0;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_GamePiece03(){
+        parse("GamePiece[4] x; x[2] = \"test\";;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_GamePiece04(){
+        parse("GamePiece[4] x; x[2] = (2,3,3);;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_GamePiece05(){
+        parse("GamePiece[4] x; x[2] = false;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_vector01(){
+        parse("vector[4] x; x[2] = 1;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_vector02(){
+        parse("vector[4] x; x[2] = 2.0;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_vector03(){
+        parse("vector[4] x; x[2] = \"test\";;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_vector04(){
+        parse("vector[4] x; GamePiece gp; x[2] = gp;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty_assignment_vector05(){
+        parse("vector[4] x; x[2] = true;;");
+    }
+
+    @Test (expected = Exception.class)
+    public void faulty01(){
+        parse("int[4] x; int i; i = x[2].test;;");
     }
 }
