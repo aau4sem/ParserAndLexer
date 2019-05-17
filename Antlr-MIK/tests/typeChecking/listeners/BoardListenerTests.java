a44c883999d8b03e7fdcab3e9c225a05eddd3475
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-05-17 17:14:24 +0200

    BoardListenerTests: completed tests for "all" cases.

--- Antlr-MIK/tests/typeChecking/listeners/BoardListenerTests.java
+++ Antlr-MIK/tests/typeChecking/listeners/BoardListenerTests.java
@@ -8,9 +8,23 @@ import static testUtilities.TestUtils.parse;
 public class BoardListenerTests {
 
     @Test
-    public void overallTest01(){
+    public void assignment01(){
         parse("Board = \"TestZero\";;");
 
         Assert.assertEquals("TestZero", bl.getBoardPath());
+    }
+
+    @Test
+    public void assignment02() {
+        parse("Board = " + "\"" + "\\some\\path\\pictures1.jpg" + "\"" + ";;");
+
+        Assert.assertEquals("\\some\\path\\pictures1.jpg", bl.getBoardPath());
+    }
+
+    @Test
+    public void assignment03() {
+        parse("Board = " + "\"" + "https://pbs.twimg.com/profile_images/1115659011330719744/FRmIw6uM.png" + "\"" + ";;");
+
+        Assert.assertEquals("https://pbs.twimg.com/profile_images/1115659011330719744/FRmIw6uM.png", bl.getBoardPath());
     }
 }
